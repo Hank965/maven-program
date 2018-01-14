d106f4f55075b645e74c5b0b8251f6229043ccff
zheng.hao <1003818871@qq.com>
2018-01-14 16:13:42 +0800

    行不行

---
+++ zhoa-basic/src/main/java/Gsxs.java
@@ -0,0 +1,11 @@
+/**
+ * Description: <BR>
+ * <p>
+
+ * @date 2018/1/14 16:13
+ */
+public class Gsxs {
+    public static void main(String[] args) {
+        System.out.println("xsaxss");
+    }
+}
